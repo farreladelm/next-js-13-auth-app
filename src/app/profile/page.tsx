2914696c470caefc15a6b13431d238c3591e50a5
Farrel Adel Mohammad <farrel.adel@gmail.com>
2023-10-13 22:23:21 +0700

    update gitignore

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,34 @@
+"use client"
+import axios from "axios"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import {toast} from "react-hot-toast"
+
+
 export default function profilePage() {
+    const router = useRouter()
+    const logout = async () => {
+        try {
+            await axios.get("/api/user/logout")
+            toast.success("success")
+            router.push("/login")
+        } catch (error: any) {
+            console.log(error.message)
+
+        }
+    }
     return (
         <>
             <div className="flex justify-center items-center min-h-screen w-screen">
-                <h1 className="p-4 text-center text-2xl font-bold">Profile Page</h1>
+                <div>
+                    <h1 className="p-4 text-center text-2xl font-bold">Profile Page</h1>
+                </div>
+                <div>
+                    <button className="flex gap-2 justify-center items-center px-4 py-2 bg-slate-950 hover:bg-slate-800 focus:bg-slate-800 border-2 border-solid border-slate-300 rounded"
+                    onClick={logout}>Logout</button>
+                </div>
             </div>
+
         </>
     )
 }
